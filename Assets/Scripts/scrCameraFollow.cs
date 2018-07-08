13ec45e6510f4be4c2f110a388d83107e297bd14
Ryan <rmcdonald19@qub.ac.uk>
2018-07-08 15:39:52 +0100

    mouse movement

---
+++ Assets/Scripts/scrCameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrCameraFollow : MonoBehaviour
+{
+    Vector3 offset;
+    public GameObject mouse;
+
+    void Start()
+    {
+        offset = new Vector3(0f, 0f, -1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = mouse.transform.position + offset;
+    }
+}
